fddfc77c160c35ac456937fcfa5bbb4725c40359
alvihasan1 <alvizaidi2@gmail.com>
2021-09-08 00:36:36 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+primaryColor: Colors.blueAccent,
+accentColor: Colors.amber,
+      ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Flutter App")
+          ) ,
+          body: Container(
+            color: Colors.amber,
+            width: 800,
+            height: 200,
+            
+            child: Text("Hello Flutter", textAlign: TextAlign.center,)
+            ),
+
+        ),
+
+    );
+  }
+}
